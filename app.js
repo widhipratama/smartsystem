afa7ea408d5c7fe06814dabdb6403b83cae13448
widhipratama <widhipratama52@gmail.com>
2021-06-24 16:52:39 +0800

    new

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express();
+var path = require("path");
+const bodyParser = require("body-parser");
+const router = require("./routes/index");
+const flash = require("connect-flash");
+const session = require("express-session");
+const cookieParser = require("cookie-parser");
+const siswa = require("./routes/siswa");
+
+app.use(cookieParser("keyboard cat"));
+app.use(session({ cookie: { maxAge: 60000 } }));
+app.use(flash());
+
+app.set("views", __dirname + "/views/");
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  "/script-stisla",
+  express.static(path.join(__dirname, "/admin-stisla/"))
+);
+app.use(function (req, res, next) {
+  res.locals.stuff = {
+    url: req.originalUrl,
+  };
+  next();
+});
+
+//body parser
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
+
+// routes
+app.use("/", router);
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`Server starts on ${port}`));
---
+++ routes/index.js
@@ -0,0 +1,9 @@
+const router = require("express").Router();
+const models = require("../models");
+const Op = require("sequelize").Op;
+
+router.get("/", (req, res) => {
+  res.render("dashboard/index");
+});
+
+module.exports = router;
