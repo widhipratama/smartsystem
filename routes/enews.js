c5d27b9e0769a259d14bbb75f5284a7e55ff9b12
Ari Surya <putra.kadek.ari@gmail.com>
2021-07-01 05:44:48 +0800

    Update Module E^News - Create View E-News

--- routes/enews.js
+++ routes/enews.js
@@ -2,6 +2,8 @@ const router = require("express").Router();
 var enewsController = require("../controllers/enewsController.js");
 
 router.get("/", enewsController.index);
+router.get("/view", enewsController.view);
+router.get("/pdf", enewsController.pdf);
 router.get("*", enewsController.notFound);
 
 module.exports = router;
