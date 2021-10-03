c726d11d39b1e0644ea3bbcf5366cb134dcb8cdb
widhipratama <widhipratama52@gmail.com>
2021-10-03 14:20:50 +0800

    update master kendaraan

--- routes/jobHistory.js
+++ routes/jobHistory.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const upload = require("../middleware/upload");
 const jobHistoryController = require("../controllers/jobHistoryController");
 
-router.post("/upload", upload.single("file"), jobHistoryController.upload);
+router.post("/upload", upload.single("image_mobil"), jobHistoryController.upload);
 router.get("/import", jobHistoryController.import);
 
 module.exports = router;
